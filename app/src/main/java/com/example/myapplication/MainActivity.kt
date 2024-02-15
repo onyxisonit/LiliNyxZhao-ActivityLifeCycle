188d877001f0ea3afde24bad208c87e5d777001f
Nyx <lilizhao107513@gmail.com>
2024-02-14 20:45:23 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+private const val TAG = "MainActivity"
+class MainActivity : AppCompatActivity() {
+    private lateinit var button: Button
+    private lateinit var textview: TextView
+    private lateinit var editText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
+        setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        textview = findViewById(R.id.textview)
+        editText = findViewById(R.id.edittext)
+
+        button.setOnClickListener {
+            textview.text = "Hello"
+            editText.setText("Hello")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+}
